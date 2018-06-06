cdd5b07eb094f939f7fc394465981820a73298aa
Ben <ben@notionparallax.co.uk>
2018-06-06 20:04:47 +1000

    Remove digraph and add some circles

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ function fiddle_about(){
     var nodes = g._groups[0][0];
     console.log(nodes);
   };
-  
+
 function whereIsEverything(){
     n = document.getElementsByClassName("node");
     d = {};
-  
+
     Array.from(n).forEach(function(item) {
         let e = item.getElementsByTagName("ellipse")[0];
   	let this_node = {};
@@ -30,123 +30,6 @@ console.log(d);
 return d;
 };
 
-var digraph_details = ['digraph G {',
- ' rankdir  = "LR";',
-    ' label    = "Tech Tree of OATMEAL Projects";',
-    ' labelloc = "t";',
-    ' node [style=filled,',
-    '       color="black", ',
-    '       fontcolor="white", ',
-    '       shape="oval", ',
-    '       fixedsize=true, ',
-    '       width=1.5,',
-    '       height=1.5];',
-    ' ',
-    ' potPaper [label="A Plane of Thrones\nPaper"];',
-    ' robPaper [label="Robotics Papers"];',
-    ' deskSenseDn [label="Below Desk Sensing"];',
-    ' desks [label="Unit Desks"];',
-    ' deskSenseUp [label="Above Desk Sensing"];',
-    ' mApp [label="Mobile App"];',
-    ' geoEthno2 [label="Geographic/\nEthnographic data 2"];',
-    ' basicBeacon [label="iBeacon Awareness"];',
-    ' sensicorn2 [label="Sensicorn 2"];',
-    ' geoEthno1 [label="Geographic/\nEthnographic data 1"];',
-    ' emailData [label="Email Data"];',
-    ' RBTM [label="Robotic Building\nTopology Modification"];',
-    ' basicRobot [label="Awareness of Robots"];',
-    ' timeData [label="Timesheet Data"];',
-    ' SNA [label="Social Network\nAnalysis"];',
-    ' changeSpace [label="Change Space\nCollective"];',
-    ' robDesk [label="Robotic Desk\nArrangement"];',
-    ' Shakedown [label="Shakedown data\ncapture"];',
-    ' SNAawareness [label="SNA Awareness"];',
-    ' phone [label="Access To Phones"];',
-    ' halo [label="Robotic Weaving"];',
-    ' SNAblog [label="Social Network\nAnalysis Blog Post"];',
-    ' SydStudioSeating [label="Sydney Studio Seating"];',
-    ' revit2ML [label="Revit Layout\nExtraction"];',
-    ' data [label="Data Literacy"];',
-    ' sensicorn1 [label="Sensicorn 1"];',
-    ' SydStudioSeating2 [label="Better Sydney Studio\nSeating"];',
-    ' diary [label="Diary Studies"];',
-    ' SLAM [label="SLAM (Simultanious\nLocation And Mapping)"];',
-    ' bravo [label="Bravo Victor November"];',
-    ' pot [label="A Plane of Thrones\nFramework"];',
-    ' deskLoc [label="Desk Location"];',
-    ' electron [label="Desktop App"];',
-    ' robSense [label="Robot Mounted Sensing"];',
-    ' boom [label="Distribution Boom"];',
-    ' altSpace [label="altSpace"];',
-    ' AIlayout [label="AI Layout"];',
-    ' lAmp [label="Learning Amplified\nSeating Plans"];',
-    ' bizCap [label="Capturing Business\nMetrics"];',
-    ' loTraining [label="Layout Training Data"];',
-    ' cadd [label="Continuous Analysis,\nDesign & Delivery"];',
-    ' geoEthno3 [label="Geographic/\nEthnographic data 3"];',
-    ' ShakedownPaper [label="Shakedown Paper"];',
-    ' s3 [label="S3"];',
-    ' sensors [label="Sensor Literacy"];',
-    ' pathVis [label="Path Visualisation"];',
-    ' lExp [label="Learning Experiment"];',
-    ' systemReef [label="Systems Reef"];',
-    ' ShakedownBlog [label="Shakedown Blog"];',
-    ' Shakedown2 [label="Shakedown (with\nstalls) Product"];',
-    ' hardware [label="Hardware Design"];',
-    ' BTARATA [label="Buildings That Are\nRight All The Time"];',
-    ' potPaper -> pot;',
-    ' potPaper -> revit2ML;',
-    ' robPaper -> halo;',
-    ' deskSenseUp -> electron;',
-    ' deskSenseUp -> s3;',
-    ' mApp -> phone;',
-    ' geoEthno2 -> geoEthno1;',
-    ' geoEthno2 -> deskSenseDn;',
-    ' sensicorn2 -> mApp;',
-    ' sensicorn2 -> sensicorn1;',
-    ' geoEthno1 -> deskLoc;',
-    ' geoEthno1 -> deskSenseUp;',
-    ' geoEthno1 -> diary;',
-    ' RBTM -> robDesk;',
-    ' SNA -> emailData;',
-    ' SNA -> timeData;',
-    ' robDesk -> AIlayout;',
-    ' robDesk -> lAmp;',
-    ' robDesk -> robSense;',
-    ' Shakedown -> sensors;',
-    ' halo -> basicRobot;',
-    ' SNAblog -> bravo;',
-    ' SydStudioSeating -> pot;',
-    ' sensicorn1 -> hardware;',
-    ' sensicorn1 -> pathVis;',
-    ' SydStudioSeating2 -> lAmp;',
-    ' SydStudioSeating2 -> SydStudioSeating;',
-    ' diary -> electron;',
-    ' deskLoc -> basicBeacon;',
-    ' deskLoc -> desks;',
-    ' robSense -> basicRobot;',
-    ' robSense -> sensors;',
-    ' robSense -> SLAM;',
-    ' boom -> systemReef;',
-    ' AIlayout -> loTraining;',
-    ' lAmp -> lExp;',
-    ' lAmp -> pot;',
-    ' lAmp -> SNA;',
-    ' cadd -> bizCap;',
-    ' cadd -> geoEthno3;',
-    ' cadd -> RBTM;',
-    ' geoEthno3 -> sensicorn2;',
-    ' geoEthno3 -> geoEthno2;',
-    ' ShakedownPaper -> Shakedown;',
-    ' s3 -> hardware;',
-    ' s3 -> sensors;',
-    ' sensors -> data;',
-    ' lExp -> revit2ML;',
-    ' Shakedown2 -> Shakedown;',
-    ' BTARATA -> cadd;',
- ' {rank = same; data; basicRobot; hardware; systemReef; SNAawareness}',
-    ' \n}'].join("\n");
-   
 
 var graph = d3.select("#graph").graphviz();
 
@@ -161,12 +44,56 @@ function getNodes(){
         d3.select(this).select("ellipse").style("fill", "black")
     });
     nodes.each(function (d) {
-        console.log(d)
-        var ellip = d3.select(this).selectAll("ellipse").clone();
-        ellip_x = ellip.style("fill", "red");
-        ellip_x = ellip_x.attr("cx", parseInt(ellip.attr("cx"), 10) + 10)
-        ellip_x = ellip_x.attr("cy", parseInt(ellip.attr("cy"), 10) + 10)
-        this.append(ellip_x)
+        // console.log(d)
+        let mainEllip = d3.select(this).select("ellipse");
+        let cx = parseInt(mainEllip.attr("cx"), 10);
+        let cy = parseInt(mainEllip.attr("cy"), 10);
+        let rx = parseInt(mainEllip.attr("rx"), 10);
+        let ry = parseInt(mainEllip.attr("ry"), 10);
+
+        //Draw the ghostly line
+        let ghostEllip = mainEllip.clone();
+        ghostEllip.style("fill", "none");
+        ghostEllip.style("stroke", "black");
+        ghostEllip.style("opacity", 0.5);
+        ghostEllip.style("stroke-width", "1");
+        ghostEllip.attr("rx", rx + 5)
+        ghostEllip.attr("ry", ry + 5)
+        ghostEllip.attr("class", "ghost-ring");
+
+        //Draw the progress bar
+        let prog_full = ghostEllip.node().getTotalLength();
+        var progressEllip = mainEllip.clone();
+        progressEllip.style("fill", "none");
+        progressEllip.style("stroke", "yellow");
+        progressEllip.style("stroke-width", "10");
+        progressEllip.style("stroke-dasharray", prog_full*Math.random());
+        progressEllip.style("stroke-dashoffset", prog_full);
+        progressEllip.style("stroke-linecap", "round");
+        progressEllip.style("animation", "dash 1s ease-out forwards");
+        progressEllip.attr("rx", rx + 5)
+        progressEllip.attr("ry", ry + 5)
+        progressEllip.attr("class", "progress-ring");
+
+        this.append(ghostEllip);
+        this.append(progressEllip);
+
+        //Draw the circles for ownership faces
+        //This needs a better way of getting the images into the circles
+        let randInt = Math.floor(Math.random() * Math.floor(4)) + 1;
+        for (var i = 0; i < randInt; i++) {
+          var ownerEllip = mainEllip.clone();
+          ownerEllip.style("fill", "red");
+          ownerEllip.style("stroke", "black");
+          let faceRad = 20;
+          ownerEllip.attr("cx", cx - (rx - faceRad) + (faceRad * 0.8 * i));
+          ownerEllip.attr("cy", cy + (ry - faceRad));
+          ownerEllip.attr("rx", faceRad);
+          ownerEllip.attr("ry", faceRad);
+          ownerEllip.attr("class", "face-ring");
+          this.append(ownerEllip);
+        }
+
     });
 
 }
@@ -181,8 +108,8 @@ function render(dotSrc) {
         .transition(transition1)
         .renderDot(dotSrc, getNodes);
 
-    
-    
+
+
 
 }
 
