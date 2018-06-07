9d06dc197278ed29e56ed082f18877d79038758c
Ben <ben@notionparallax.co.uk>
2018-06-07 22:21:55 +1000

    stying :nail_care:

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Written by Ben Doherty June 2018
 
-function Get(yourUrl){
-    var Httpreq = new XMLHttpRequest(); // a new request
-    Httpreq.open("GET",yourUrl,false);
-    Httpreq.send(null);
-    return Httpreq.responseText;
-}
+// function Get(yourUrl){
+//     var Httpreq = new XMLHttpRequest(); // a new request
+//     Httpreq.open("GET",yourUrl,false);
+//     Httpreq.send(null);
+//     return Httpreq.responseText;
+// }
 
 
 function fiddle_about(){
@@ -41,24 +41,24 @@ return d;
 var graph = d3.select("#graph").graphviz();
 
 function getNodes(){
-    var project_json_obj = JSON.parse(Get("parallellData.json"));
-
     // console.log(d3.selectAll('.node'));
     nodes = d3.selectAll('.node');
 
     nodes.on("mouseover", function (d) {
-        // console.log(cx, cy);
         d3.select(this).select("ellipse").style("fill", "blue")
     }).on("mouseout", function (d) {
         d3.select(this).select("ellipse").style("fill", "black")
     });
     nodes.each(function (d) {
       var base = 10;
-      var ringOffset = 8;
+      var ringOffset = 6;
       var faceRad = 10;
+      var pd = parallellData[d.key];
 
       console.log(d)
       var mainEllip = d3.select(this).select("ellipse");
+
+
       var cx = parseInt(mainEllip.attr("cx"), base);
       var cy = parseInt(mainEllip.attr("cy"), base);
       var rx = parseInt(mainEllip.attr("rx"), base);
@@ -69,19 +69,21 @@ function getNodes(){
       ghostEllip.attr("rx", rx + (ringOffset*0.5));
       ghostEllip.attr("ry", ry + (ringOffset*0.5));
       ghostEllip.attr("class", "ghost-ring");
+      this.append(ghostEllip);
 
       //Draw the progress bar
-      console.log(project_json_obj[d.key].percentComplete * 0.01);
-      var prog_full = ghostEllip.node().getTotalLength();
-      var progressEllip = mainEllip.clone();
-      progressEllip.style("stroke-dasharray", prog_full * project_json_obj[d.key].percentComplete * 0.01);
-      progressEllip.style("stroke-dashoffset", prog_full);
-      progressEllip.attr("rx", rx + (ringOffset*0.5))
-      progressEllip.attr("ry", ry + (ringOffset*0.5))
-      progressEllip.attr("class", "progress-ring");
-
-      this.append(ghostEllip);
-      this.append(progressEllip);
+      if (pd.percentComplete > 0) {
+        var prog_full = ghostEllip.node().getTotalLength();
+        var prog = prog_full * ((100 - pd.percentComplete) / 100);
+        var progressEllip = mainEllip.clone();
+        progressEllip.style("stroke-dasharray", prog_full);
+        progressEllip.style("stroke-dashoffset", Math.max(0, prog));
+        progressEllip.style("stroke-width", ringOffset);
+        progressEllip.attr("rx", rx + (ringOffset*0.5))
+        progressEllip.attr("ry", ry + (ringOffset*0.5))
+        progressEllip.attr("class", "progress-ring");
+        this.append(progressEllip);
+      }
 
       //Draw the circles for ownership faces
       //This needs a better way of getting the images into the circles
@@ -94,6 +96,21 @@ function getNodes(){
         ownerEllip.attr("ry", faceRad);
         ownerEllip.attr("class", "face-ring");
         this.append(ownerEllip);
+      }
+
+      //set node colours based on type of node
+      if (pd.Type === "Project") {
+        mainEllip.style("fill", "red");
+      } else if (pd.Type === "Capability"){
+        mainEllip.style("fill", "green");
+      } else if (pd.Type === "Publication"){
+        mainEllip.style("fill", "blue");
+      } else if (pd.Type === "Milestone"){
+        mainEllip.style("fill", "orange");
+      } else if (pd.Type === "Product"){
+        mainEllip.style("fill", "silver");
+      } else {
+        //nothing
       }
 
   });
