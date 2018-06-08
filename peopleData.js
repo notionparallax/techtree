53a16a33b1d5e7330b36244fa2933a6e9bbb413d
Ben <ben@notionparallax.co.uk>
2018-06-08 14:40:11 +1000

    new data

---
+++ peopleData.js
@@ -0,0 +1,472 @@
+var peopleData = {
+  "rec0dpxUhHfgHWU7r": {
+    "Headshot": [
+      {
+        "filename": "P_160405_N137.jpg",
+        "id": "attIji1HbmalXUkuZ",
+        "size": 186823,
+        "thumbnails": {
+          "large": {
+            "height": 500,
+            "url": "https://dl.airtable.com/6ax7C3bmSUWbWYULMZsZ_large_P_160405_N137.jpg",
+            "width": 500
+          },
+          "small": {
+            "height": 36,
+            "url": "https://dl.airtable.com/mJbE3QgJRMyJfWsJkFDs_small_P_160405_N137.jpg",
+            "width": 36
+          }
+        },
+        "type": "image/jpeg",
+        "url": "https://dl.airtable.com/XDqFqCuSQTKj0p03atm9_P_160405_N137.jpg"
+      }
+    ],
+    "Initials": "KM",
+    "Name": "Kevin M",
+    "Notes": null,
+    "Techs": [
+      "recnXgQGgzh4q3t9M"
+    ],
+    "id": "rec0dpxUhHfgHWU7r",
+    "thumb_large": "https://dl.airtable.com/6ax7C3bmSUWbWYULMZsZ_large_P_160405_N137.jpg"
+  },
+  "rec3HypnJiuTkeVBJ": {
+    "Headshot": [
+      {
+        "filename": "P_160405_N20.jpg",
+        "id": "attBViPOM4doUlrmU",
+        "size": 170959,
+        "thumbnails": {
+          "large": {
+            "height": 500,
+            "url": "https://dl.airtable.com/JgeHAo9xSGmjVWy0FtXS_large_P_160405_N20.jpg",
+            "width": 500
+          },
+          "small": {
+            "height": 36,
+            "url": "https://dl.airtable.com/8QWYcVKNR2iXQcBUCqAq_small_P_160405_N20.jpg",
+            "width": 36
+          }
+        },
+        "type": "image/jpeg",
+        "url": "https://dl.airtable.com/mbzSbvBfSFKL7o35bJf5_P_160405_N20.jpg"
+      }
+    ],
+    "Initials": "ARR",
+    "Name": "Annisa",
+    "Notes": null,
+    "Techs": [
+      "recru4UXYdMmO7sX8"
+    ],
+    "id": "rec3HypnJiuTkeVBJ",
+    "thumb_large": "https://dl.airtable.com/JgeHAo9xSGmjVWy0FtXS_large_P_160405_N20.jpg"
+  },
+  "recF9tgC6LJUmi5gJ": {
+    "Headshot": [
+      {
+        "filename": "dura-ace-cog.jpg",
+        "id": "attzVBMKsBoqCwH4N",
+        "size": 139753,
+        "thumbnails": {
+          "large": {
+            "height": 512,
+            "url": "https://dl.airtable.com/JDhVYh9SAOPYoIBE6ipb_large_dura-ace-cog.jpg",
+            "width": 614
+          },
+          "small": {
+            "height": 36,
+            "url": "https://dl.airtable.com/Nb21kuBJTLcWQK4XkRuw_small_dura-ace-cog.jpg",
+            "width": 43
+          }
+        },
+        "type": "image/jpeg",
+        "url": "https://dl.airtable.com/4lBScRgSwycEkslVUVcO_dura-ace-cog.jpg"
+      }
+    ],
+    "Initials": "--",
+    "Name": "nobodyAssigned",
+    "Notes": "This is who'll be assigned automagically if nobody is assigned",
+    "Techs": null,
+    "id": "recF9tgC6LJUmi5gJ",
+    "thumb_large": "https://dl.airtable.com/JDhVYh9SAOPYoIBE6ipb_large_dura-ace-cog.jpg"
+  },
+  "recFKWrjwOUM5oWxI": {
+    "Headshot": [
+      {
+        "filename": "P_160405_N26.jpg",
+        "id": "attJPKI8qeNxWFgDi",
+        "size": 159204,
+        "thumbnails": {
+          "large": {
+            "height": 500,
+            "url": "https://dl.airtable.com/4NWLSnTFRwSfdFDBAt9A_large_P_160405_N26.jpg",
+            "width": 500
+          },
+          "small": {
+            "height": 36,
+            "url": "https://dl.airtable.com/0nZTz4SMGjAzgKSWDe5g_small_P_160405_N26.jpg",
+            "width": 36
+          }
+        },
+        "type": "image/jpeg",
+        "url": "https://dl.airtable.com/rjQbMqmUQpKBu2MyCd5X_P_160405_N26.jpg"
+      }
+    ],
+    "Initials": "BaD",
+    "Name": "Barry D",
+    "Notes": null,
+    "Techs": null,
+    "id": "recFKWrjwOUM5oWxI",
+    "thumb_large": "https://dl.airtable.com/4NWLSnTFRwSfdFDBAt9A_large_P_160405_N26.jpg"
+  },
+  "recFUgthTUYQnR7Mz": {
+    "Headshot": null,
+    "Initials": "RX",
+    "Name": "AI Richard",
+    "Notes": null,
+    "Techs": null,
+    "id": "recFUgthTUYQnR7Mz",
+    "thumb_large": "http://getdrawings.com/images/casper-drawing-8.jpg"
+  },
+  "recHLT6IoCZXwwtp3": {
+    "Headshot": [
+      {
+        "filename": "P_160405_N190.jpg",
+        "id": "att05xuQ0g4VVK3Nh",
+        "size": 145252,
+        "thumbnails": {
+          "large": {
+            "height": 500,
+            "url": "https://dl.airtable.com/n5K9IIPLSEiPPcY8qKCG_large_P_160405_N190.jpg",
+            "width": 500
+          },
+          "small": {
+            "height": 36,
+            "url": "https://dl.airtable.com/kMYbSN7TneqIjt1MUuth_small_P_160405_N190.jpg",
+            "width": 36
+          }
+        },
+        "type": "image/jpeg",
+        "url": "https://dl.airtable.com/rvs1ofaTQ0KuslEhniYh_P_160405_N190.jpg"
+      }
+    ],
+    "Initials": "NT",
+    "Name": "Ninotschka",
+    "Notes": null,
+    "Techs": [
+      "rec0tbY526abDXcRr"
+    ],
+    "id": "recHLT6IoCZXwwtp3",
+    "thumb_large": "https://dl.airtable.com/n5K9IIPLSEiPPcY8qKCG_large_P_160405_N190.jpg"
+  },
+  "recHN7P3Wz3YGdAN2": {
+    "Headshot": [
+      {
+        "filename": "P_160405_N31.jpg",
+        "id": "att6DDYg3qurTxHLC",
+        "size": 54300,
+        "thumbnails": {
+          "large": {
+            "height": 500,
+            "url": "https://dl.airtable.com/nestrTYmQqT79f6rfpHQ_large_P_160405_N31.jpg",
+            "width": 500
+          },
+          "small": {
+            "height": 36,
+            "url": "https://dl.airtable.com/96Z81gBcS8uwD5dCSEww_small_P_160405_N31.jpg",
+            "width": 36
+          }
+        },
+        "type": "image/jpeg",
+        "url": "https://dl.airtable.com/psrBb8e4Qbpb9g5PrMAF_P_160405_N31.jpg"
+      }
+    ],
+    "Initials": "BiD",
+    "Name": "Bill",
+    "Notes": null,
+    "Techs": [
+      "recmNHHmIUj4nPild",
+      "recHpePW60ZlXrLKY"
+    ],
+    "id": "recHN7P3Wz3YGdAN2",
+    "thumb_large": "https://dl.airtable.com/nestrTYmQqT79f6rfpHQ_large_P_160405_N31.jpg"
+  },
+  "recMZdSRhVMMJQcOb": {
+    "Headshot": [
+      {
+        "filename": "P_160405_N25.jpg",
+        "id": "attG3o5l2IdGJbkhh",
+        "size": 134490,
+        "thumbnails": {
+          "large": {
+            "height": 500,
+            "url": "https://dl.airtable.com/tEyLZYnrSSaN4Ikic0k5_large_P_160405_N25.jpg",
+            "width": 500
+          },
+          "small": {
+            "height": 36,
+            "url": "https://dl.airtable.com/alB8IJyHRHiVUO6oZvS4_small_P_160405_N25.jpg",
+            "width": 36
+          }
+        },
+        "type": "image/jpeg",
+        "url": "https://dl.airtable.com/LO2GzIePRpexTNeifQm0_P_160405_N25.jpg"
+      }
+    ],
+    "Initials": "BC",
+    "Name": "Barry C",
+    "Notes": null,
+    "Techs": [
+      "recqsdnZyKEXLlafN"
+    ],
+    "id": "recMZdSRhVMMJQcOb",
+    "thumb_large": "https://dl.airtable.com/tEyLZYnrSSaN4Ikic0k5_large_P_160405_N25.jpg"
+  },
+  "rece9qyNSfMqAhZAL": {
+    "Headshot": [
+      {
+        "filename": "MVIMG_20180224_173642.jpg",
+        "id": "attiDpgZgGjEWT4iR",
+        "size": 7441317,
+        "thumbnails": {
+          "large": {
+            "height": 683,
+            "url": "https://dl.airtable.com/nUBlmdvR1iLT0mCbllA1_large_MVIMG_20180224_173642.jpg",
+            "width": 512
+          },
+          "small": {
+            "height": 36,
+            "url": "https://dl.airtable.com/vao3Ge65RXuQs17gLU4p_small_MVIMG_20180224_173642.jpg",
+            "width": 27
+          }
+        },
+        "type": "image/jpeg",
+        "url": "https://dl.airtable.com/i6xzpJ9QSPmRqqlhxxYH_MVIMG_20180224_173642.jpg"
+      },
+      {
+        "filename": "face.jpg",
+        "id": "att24vtQ3Nyb0Et7F",
+        "size": 550035,
+        "thumbnails": {
+          "large": {
+            "height": 512,
+            "url": "https://dl.airtable.com/Xuhm9DnPRXaH8Cb6U7B5_large_face.jpg",
+            "width": 524
+          },
+          "small": {
+            "height": 36,
+            "url": "https://dl.airtable.com/4oVo2DG0R8uoLJ96MlNA_small_face.jpg",
+            "width": 37
+          }
+        },
+        "type": "image/jpeg",
+        "url": "https://dl.airtable.com/Qn78TSlQQyitfoeaYFwX_face.jpg"
+      }
+    ],
+    "Initials": "BeD",
+    "Name": "Ben",
+    "Notes": null,
+    "Techs": [
+      "reccHCPeML8SyRFTp",
+      "rec5v7YBAh4XzeOQ7",
+      "recSIIp1grVggDGQo",
+      "recqsdnZyKEXLlafN",
+      "reco8NZ3WlsKp4wJg"
+    ],
+    "id": "rece9qyNSfMqAhZAL",
+    "thumb_large": "https://dl.airtable.com/nUBlmdvR1iLT0mCbllA1_large_MVIMG_20180224_173642.jpg"
+  },
+  "recgcDkgKvrBLH1k0": {
+    "Headshot": [
+      {
+        "filename": "Ishaan.jpg",
+        "id": "atthfGGFkTCJEDwUT",
+        "size": 1356236,
+        "thumbnails": {
+          "large": {
+            "height": 831,
+            "url": "https://dl.airtable.com/hI2scvcS1WedienZx2uw_large_Ishaan.jpg",
+            "width": 512
+          },
+          "small": {
+            "height": 36,
+            "url": "https://dl.airtable.com/4GxL57iRQGHepMnMgl7u_small_Ishaan.jpg",
+            "width": 22
+          }
+        },
+        "type": "image/jpeg",
+        "url": "https://dl.airtable.com/UpUIIl24SauNKHDBRDsg_Ishaan.jpg"
+      }
+    ],
+    "Initials": "IV",
+    "Name": "Ishaan",
+    "Notes": null,
+    "Techs": [
+      "recCdV0IBu62AhiOt",
+      "recmjo5fVtiAflmvo",
+      "recflCm8BzuGQbmW0",
+      "rec0ArDcPS4hLrmJq",
+      "recFT5OW9AN6NJAAp"
+    ],
+    "id": "recgcDkgKvrBLH1k0",
+    "thumb_large": "https://dl.airtable.com/hI2scvcS1WedienZx2uw_large_Ishaan.jpg"
+  },
+  "recj2rmnndUmxB3Dt": {
+    "Headshot": [
+      {
+        "filename": "50.png",
+        "id": "attE4V4KrVsP8E7wd",
+        "size": 4270,
+        "thumbnails": {
+          "large": {
+            "height": 50,
+            "url": "https://dl.airtable.com/wNAkSR0RUlLTf8AMRYJA_large_50.png",
+            "width": 50
+          },
+          "small": {
+            "height": 36,
+            "url": "https://dl.airtable.com/cU0MsTORrmdxB0n3NQgB_small_50.png",
+            "width": 36
+          }
+        },
+        "type": "image/png",
+        "url": "https://dl.airtable.com/ekDPZ0oMTaenUrQ0BnuA_50.png"
+      }
+    ],
+    "Initials": "AR",
+    "Name": "Aiden",
+    "Notes": null,
+    "Techs": [
+      "rec5v7YBAh4XzeOQ7",
+      "rec2musyOg3qL274O",
+      "reczMCua56WUmp3zF",
+      "recqsdnZyKEXLlafN",
+      "recrOomL72JDA45yp",
+      "reco8NZ3WlsKp4wJg"
+    ],
+    "id": "recj2rmnndUmxB3Dt",
+    "thumb_large": "https://dl.airtable.com/wNAkSR0RUlLTf8AMRYJA_large_50.png"
+  },
+  "recjdvfsS0ZXESR2L": {
+    "Headshot": [
+      {
+        "filename": "Shawnee+Finlayson.jpg",
+        "id": "attF0UqrKqXac5UAR",
+        "size": 1836976,
+        "thumbnails": {
+          "large": {
+            "height": 740,
+            "url": "https://dl.airtable.com/QwBnkwoAT86QmEXcI58t_large_Shawnee%2BFinlayson.jpg",
+            "width": 512
+          },
+          "small": {
+            "height": 36,
+            "url": "https://dl.airtable.com/WZErzSZoS6q8KwMj0n6A_small_Shawnee%2BFinlayson.jpg",
+            "width": 25
+          }
+        },
+        "type": "image/jpeg",
+        "url": "https://dl.airtable.com/P2n4NG1pSnqBhuzjkB7o_Shawnee%2BFinlayson.jpg"
+      }
+    ],
+    "Initials": "SF",
+    "Name": "Shawnee",
+    "Notes": null,
+    "Techs": [
+      "recmUQLIxzr2wiyzd"
+    ],
+    "id": "recjdvfsS0ZXESR2L",
+    "thumb_large": "https://dl.airtable.com/QwBnkwoAT86QmEXcI58t_large_Shawnee%2BFinlayson.jpg"
+  },
+  "reck4uCQdMmA9Dy0e": {
+    "Headshot": [
+      {
+        "filename": "P_160405_N47.jpg",
+        "id": "attGOA5MpsB4raA3H",
+        "size": 191503,
+        "thumbnails": {
+          "large": {
+            "height": 500,
+            "url": "https://dl.airtable.com/1ocVojsCTZJYDRTtp7kI_large_P_160405_N47.jpg",
+            "width": 500
+          },
+          "small": {
+            "height": 36,
+            "url": "https://dl.airtable.com/sjGxLU8WRpGhiLXpuIoQ_small_P_160405_N47.jpg",
+            "width": 36
+          }
+        },
+        "type": "image/jpeg",
+        "url": "https://dl.airtable.com/VqHW2czEQQagyG4rDIw4_P_160405_N47.jpg"
+      }
+    ],
+    "Initials": "CB",
+    "Name": "Chris B",
+    "Notes": null,
+    "Techs": [
+      "rectkEzpAV9fxHPwS"
+    ],
+    "id": "reck4uCQdMmA9Dy0e",
+    "thumb_large": "https://dl.airtable.com/1ocVojsCTZJYDRTtp7kI_large_P_160405_N47.jpg"
+  },
+  "recuFyI25ChOwWWhr": {
+    "Headshot": [
+      {
+        "filename": "20171005_171412_resized.jpg",
+        "id": "atttBWDq0tQn5PWfU",
+        "size": 205211,
+        "thumbnails": {
+          "large": {
+            "height": 687,
+            "url": "https://dl.airtable.com/FRJXAfziQn226JiBopNq_large_20171005_171412_resized.jpg",
+            "width": 512
+          },
+          "small": {
+            "height": 36,
+            "url": "https://dl.airtable.com/isoTiRQUmHSFgQ8Iw63w_small_20171005_171412_resized.jpg",
+            "width": 27
+          }
+        },
+        "type": "image/jpeg",
+        "url": "https://dl.airtable.com/eaL9jk1iSyeknVHIsrOx_20171005_171412_resized.jpg"
+      }
+    ],
+    "Initials": "BH",
+    "Name": "Baptiste",
+    "Notes": null,
+    "Techs": [
+      "rec5v7YBAh4XzeOQ7"
+    ],
+    "id": "recuFyI25ChOwWWhr",
+    "thumb_large": "https://dl.airtable.com/FRJXAfziQn226JiBopNq_large_20171005_171412_resized.jpg"
+  },
+  "recwl2PnrQ8ueQBQ7": {
+    "Headshot": [
+      {
+        "filename": "P_160405_N171.jpg",
+        "id": "attCl8pJkU9awSTLm",
+        "size": 160824,
+        "thumbnails": {
+          "large": {
+            "height": 500,
+            "url": "https://dl.airtable.com/yXqIqKWoQcWED51FYjOO_large_P_160405_N171.jpg",
+            "width": 500
+          },
+          "small": {
+            "height": 36,
+            "url": "https://dl.airtable.com/uwMGu8v4RUqBN2gU1kN0_small_P_160405_N171.jpg",
+            "width": 36
+          }
+        },
+        "type": "image/jpeg",
+        "url": "https://dl.airtable.com/dXxQw6E2TeeOQjRff6jS_P_160405_N171.jpg"
+      }
+    ],
+    "Initials": "MB",
+    "Name": "Matthew",
+    "Notes": null,
+    "Techs": null,
+    "id": "recwl2PnrQ8ueQBQ7",
+    "thumb_large": "https://dl.airtable.com/yXqIqKWoQcWED51FYjOO_large_P_160405_N171.jpg"
+  }
+};
