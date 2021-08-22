6a406ac6b5a69f713058cf6f70fd4be9cb9a24ca
Ben <ben@notionparallax.co.uk>
2021-08-22 17:42:48 +1000

    make things a little more robuts

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,7 @@
 import json
 import ast
 import pandas as pd
+import numpy as np
 import requests
 import textwrap
 import simplejson
@@ -14,9 +15,9 @@ TESTING = False
 
 
 def lambda_handler(event, context):
-    jsonp_body = (
-        "// This is crusty, but it seems to work.\n" f"// Generated at {datetime.now()}"
-    )
+    jsonp_body = "// This is crusty, but it seems to work.\n"
+    jsonp_body += f"// Generated at {datetime.now()}"
+
     ret_dict = {}
 
     def mergeD(ugly_data):
@@ -90,18 +91,22 @@ def lambda_handler(event, context):
 
     def wrap_long_name(row):
         """Insert line breaks to make names fit in the node."""
+        if row.LongName is not np.nan:
+            longName = row.LongName
+        else:
+            longName = row.Name
         try:
-            ln = "\n".join(textwrap.wrap(row["LongName"], LINE_LENGTH))
+            ln = "\n".join(textwrap.wrap(longName, LINE_LENGTH))
         except Exception as e:
             print(e)
-            ln = row["LongName"]
+            ln = row["Name"]
         return ln
 
     def make_node_description(row, long_name_broken):
-        node_description_pattern = '{name} [label="{longName}"];'
-        return node_description_pattern.format(
-            name=row["Name"], longName=long_name_broken
-        )
+        if long_name_broken is not np.nan:
+            return f'{row.Name} [label="{long_name_broken}"];'
+        else:
+            return f"{row.Name}"
 
     project_data = get_table("Techs")
     project_data.set_index("id", inplace=True)
@@ -113,43 +118,38 @@ def lambda_handler(event, context):
         print(row)
         group_dict[row.id] = {"name": row.Name, "longName": row.LongName, "nodes": []}
 
-    edge_pattern = "{from_node} -> {to_node};"
-
     node_descriptions = []
     edges = []
     for _, row in project_data.iterrows():
-        ln = wrap_long_name(row)
-        n = make_node_description(row, ln)
-        node_descriptions.append(n)
+        if row.Name is not np.nan:
+            ln = wrap_long_name(row)
+            n = make_node_description(row, ln)
+            node_descriptions.append(n)
 
-        if (
-            type(row["precursors"]) is not float
-        ):  # nothing reads as NaN, therefore float
-            precursors = row["precursors"]  # .split(",")
-            for precursor in precursors:
-                e = edge_pattern.format(
-                    from_node=project_data.loc[precursor].Name, to_node=row["Name"]
-                )
-                edges.append(e)
+            if row["precursors"] is not np.nan:
+                precursors = row["precursors"]  # .split(",")
+                for precursor in precursors:
+                    from_node = project_data.loc[precursor].Name
+                    to_node = row["Name"]
+                    e = f"{from_node} -> {to_node};"
+                    edges.append(e)
 
-        if type(row["Group"]) is not float:  # nothing reads as NaN, therefore float
-            id = row["Group"][0]
-            print(group_dict[id])
-            group_dict[id]["nodes"].append(row["Name"])
+            if row["Group"] is not np.nan:
+                id = row["Group"][0]
+                print(group_dict[id])
+                group_dict[id]["nodes"].append(row["Name"])
 
     # print(group_dict)
-    subgraph_template = """
-subgraph cluster_{name} {{
-    style="rounded"
-    node [style=filled, color=white];
-    {nodes};
-    label = "{long_name}";
-}}"""
+
     subgraphs = ""
     for _, v in group_dict.items():
-        subgraphs += subgraph_template.format(
-            name=v["name"], long_name=v["longName"], nodes="; ".join(v["nodes"])
-        )
+        subgraphs += f"""subgraph cluster_{v["name"]} {{
+                            style="rounded"
+                            node [style=filled, color=white];
+                            {"; ".join(v["nodes"])};
+                            label = "{v["longName"]}";
+                        }}
+                        """
 
     g = f"""digraph G {{
       size = "16.66,8.33!"; // 1200x600 at 72px/in, "!" to force
@@ -228,4 +228,4 @@ if TESTING or __name__ == "__main__":
     d = lambda_handler(1, 2)
     print(d)
     with open("static_graph.json", "w", encoding="utf-8") as f:
-        json.dump(d, f)
+        json.dump(d, f, indent=2)
